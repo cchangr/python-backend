07a0cc4c7d40730add82b88c38c76411aa19e112
cchangr <chen.changr@husky.neu.edu>
2018-11-01 19:10:21 -0400

    update routes

--- todolist/routes.py
+++ todolist/routes.py
@@ -1,3 +1,19 @@
+def route_index(request):
+    pass
+
+
+def route_login(request):
+    pass
+
+
+def route_register(request):
+    pass
+
+
+def route_message(request):
+    pass
+
+
 route_dict = {
     '/': route_index,
     '/login': route_login,
--- todolist/routes_todo.py
+++ todolist/routes_todo.py
@@ -1,3 +1,27 @@
+def index(request):
+    pass
+
+
+def edit(request):
+    pass
+
+
+def add(request):
+    pass
+
+
+def update(request):
+    pass
+
+
+def delete_todo(request):
+    pass
+
+
+def login_required(route_function):
+    pass
+
+
 route_dict = {
     # GET request, 显示页面
     '/todo': index,
