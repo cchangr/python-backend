f1bf6e0c734796a2949d88bd948c11069bea7e02
cchangr <chen.changr@husky.neu.edu>
2018-11-08 16:18:25 -0500

    build todo

--- todolist_2/utils.py
+++ todolist_2/utils.py
@@ -1,20 +1,5 @@
 import time
-from jinja2 import Environment, FileSystemLoader
-import os.path
 
 
 def log(*args, **kwargs):
-    format = '%Y/%m/%d %H:%M:%S'
-    value = time.localtime(int(time.time()))
-    dt = time.strftime(format, value)
-    with open('logh.txt', 'a', encoding='utf-8') as f:
-        print(dt, *args, file=f, **kwargs)
-
-
-path = '{}/templates/'.format(os.path.dirname(__file__))
-loader = FileSystemLoader(path)
-env = Environment(loader)
-
-template = env.get_template('demo.html')
-
-
+    pass
