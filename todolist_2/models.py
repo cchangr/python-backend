9a78d9586b5830b3afefeb3de8231b23b2f286ad
cchangr <chen.changr@husky.neu.edu>
2018-11-09 11:51:22 -0500

    update

--- todolist_2/models.py
+++ todolist_2/models.py
@@ -5,10 +5,6 @@ import time
 
 
 def save(data, path):
-    """
-    data 是 dict 或者 list
-    path 是保存文件的路径
-    """
     s = json.dumps(data, indent=2, ensure_ascii=False)
     with open(path, 'w+', encoding='utf-8') as f:
         # log('save', path, s, data)
@@ -23,36 +19,17 @@ def load(path):
 
 
 # Model 是一个 ORM（object relation mapper）
-# 好处就是不需要关心存储数据的细节，直接使用即可
 class Model(object):
-    """
-    Model 是所有 model 的基类
-    @classmethod 是一个套路用法
-    例如
-    user = User()
-    user.db_path() 返回 User.txt
-    """
     @classmethod
     def db_path(cls):
-        """
-        cls 是类名, 谁调用的类名就是谁的
-        classmethod 有一个参数是 class(这里我们用 cls 这个名字)
-        所以我们可以得到 class 的名字
-        """
         classname = cls.__name__
         path = 'data/{}.txt'.format(classname)
         return path
 
     @classmethod
     def all(cls):
-        """
-        all 方法(类里面的函数叫方法)使用 load 函数得到所有的 models
-        """
         path = cls.db_path()
         models = load(path)
-        # 这里用了列表推导生成一个包含所有 实例 的 list
-        # m 是 dict, 用 cls(m) 可以初始化一个 cls 的实例
-        # 不明白就 log 大法看看这些都是啥
         ms = [cls(m) for m in models]
         return ms
 
@@ -65,24 +42,18 @@ class Model(object):
             k, v = key, value
         all = cls.all()
         for m in all:
-            # 也可以用 getattr(m, k) 取值
             if v == m.__dict__[k]:
                 ms.append(m)
         return ms
 
     @classmethod
     def find_by(cls, **kwargs):
-        """
-        用法如下，kwargs 是只有一个元素的 dict
-        u = User.find_by(username='gua')
-        """
         log('kwargs, ', kwargs, type(kwargs))
         k, v = '', ''
         for key, value in kwargs.items():
             k, v = key, value
         all = cls.all()
         for m in all:
-            # 也可以用 getattr(m, k) 取值
             if v == m.__dict__[k]:
                 return m
         return None
@@ -99,9 +70,7 @@ class Model(object):
             if e.id == id:
                 index = i
                 break
-        # 判断是否找到了这个 id 的数据
         if index == -1:
-            # 没找到
             pass
         else:
             models.pop(index)
@@ -110,30 +79,15 @@ class Model(object):
             save(l, path)
 
     def __repr__(self):
-        """
-        __repr__ 是一个魔法方法
-        简单来说, 它的作用是得到类的 字符串表达 形式
-        比如 print(u) 实际上是 print(u.__repr__())
-        """
         classname = self.__class__.__name__
         properties = ['{}: ({})'.format(k, v) for k, v in self.__dict__.items()]
         s = '\n'.join(properties)
         return '< {}\n{} \n>\n'.format(classname, s)
 
     def save(self):
-        """
-        用 all 方法读取文件中的所有 model 并生成一个 list
-        把 self 添加进去并且保存进文件
-        """
-        # log('debug save')
         models = self.all()
-        # log('models', models)
-        # 如果没有 id，说明是新添加的元素
         if self.id is None:
-            # 设置 self.id
-            # 先看看是否是空 list
             if len(models) == 0:
-                # 我们让第一个元素的 id 为 1（当然也可以为 0）
                 self.id = 1
             else:
                 m = models[-1]
@@ -155,10 +109,6 @@ class Model(object):
 
 
 class User(Model):
-    """
-    User 是一个保存用户数据的 model
-    现在只有两个属性 username 和 password
-    """
     def __init__(self, form):
         self.id = form.get('id', None)
         self.username = form.get('username', '')
@@ -200,7 +150,6 @@ class Todo(Model):
             'completed'
         ]
         for key in form:
-            # 这里只应该更新我们想要更新的东西
             if key in valid_names:
                 setattr(t, key, form[key])
         # 修改更新时间
@@ -227,9 +176,7 @@ class Todo(Model):
         self.id = form.get('id', None)
         self.task = form.get('task', '')
         self.completed = False
-        # 和别的数据关联的方式, 用 user_id 表明拥有它的 user 实例
         self.user_id = form.get('user_id', user_id)
-        # 添加创建和修改时间
         self.created_time = form.get('created_time', None)
         self.updated_time = form.get('updated_time', None)
         if self.created_time is None:
