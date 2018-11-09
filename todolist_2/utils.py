26a5eb4905aaf6caa6c4c5bae6a2578d60eab5db
cchangr <chen.changr@husky.neu.edu>
2018-11-08 23:25:50 -0500

    update utils

--- todolist_2/utils.py
+++ todolist_2/utils.py
@@ -1,3 +1,5 @@
+from jinja2 import Environment, FileSystemLoader
+import os
 import time
 
 
@@ -5,4 +7,43 @@ def log(*args, **kwargs):
     format = '%Y/%m/%d %H:%M:%S'
     value = time.localtime(int(time.time()))
     dt = time.strftime(format, value)
-    print(dt, *args, **kwargs)
+    with open('log.txt', 'a', encoding='utf-8') as f:
+        print(dt, *args, file=f, **kwargs)
+
+
+path = '{}/templates/'.format(os.path.dirname(__file__))
+loader = FileSystemLoader(path)
+env = Environment(loader=loader)
+
+
+def template(path, **kwargs):
+    t = env.get_template(path)
+    return t.render(**kwargs)
+
+
+def response_with_headers(headers, status_code=200):
+    header = 'HTTP/1.1 {} OK\r\n'.format(status_code)
+    header += ''.join(['{}: {}\r\n'.format(k, v)
+                              for k, v in headers.items()])
+    return header
+
+
+def redirect(location, headers=None):
+    h = {
+        'Content-Type': 'text/html',
+    }
+    if headers is not None:
+        h.update(headers)
+    h['Location'] = location
+    header = response_with_headers(h, 302)
+    r = header + '\r\n' + ''
+    return r.encode(encoding='utf-8')
+
+
+def http_response(body, headers=None):
+    header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n'
+    if headers is not None:
+        header += ''.join(['{}: {}\r\n'.format(k, v)
+                            for k, v in headers.items()])
+    r = header + '\r\n' + body
+    return r.encode(encoding='utf-8')
