5983d6e0324b26a61fb9ee812b625ba76ecae989
cchangr <chen.changr@husky.neu.edu>
2018-11-19 22:28:33 -0500

    update models

--- todolist_3/models.py
+++ todolist_3/models.py
@@ -12,7 +12,7 @@ def save(data, path):
 def load(path):
     '''json.loads: Deserialize ``s`` (a ``str``, ``bytes`` or ``bytearray`` instance
        containing a JSON document) to a Python object'''
-    with open(path, 'r', encoding = 'utf-8') as f:
+    with open(path, 'r', encoding='utf-8') as f:
         s = f.read()
         return json.loads(s)
 
@@ -28,31 +28,194 @@ class Model(object):
 
     @classmethod
     def all(cls):
-       path = cls.db_path()
-       models = load(path)
-       ms = [cls(m) for m in models]
-       return ms
+        path = cls.db_path()
+        models = load(path)
+        ms = [cls(m) for m in models]
+        return ms
 
     @classmethod
     def find_all(cls, **kwargs):
-        pass
+        ms = []
+        k, v = '', ''
+        for key, value in kwargs.items():
+            key, value = k, v
+        all = cls.all()
+        for m in all:
+            if v == m.__dict__[k]:
+                ms.append(m)
+        return ms
 
     @classmethod
     def find_by(cls, **kwargs):
-        pass
+        log('kwargs, ', kwargs, type(kwargs))
+        k, v = '', ''
+        for key, value in kwargs.items():
+            k, v = key, value
+        all = cls.all()
+        for m in all:
+            # 也可以用 getattr(m, k) 取值
+            if v == m.__dict__[k]:
+                return m
+        return None
 
     @classmethod
     def find(cls, id):
-        pass
+        return cls.find_by(id=id)
 
     @classmethod
     def delete(cls, id):
-        pass
+        models = cls.all()
+        index = -1
+        for i, e in enumerate(models):
+            if e.id == id:
+                index = i
+                break
+            #
+        if index == -1:
+            #
+            pass
+        else:
+            models.pop(index)
+            l = [m.__dict__ for m in models]
+            path = cls.db_path()
+            save(l, path)
+            return
 
     def __repr__(self):
-        pass
+        classname = self.__class__.__name__
+        properties = ['{}: ({})'.format(k, v) for k, v in self.__dict__.items()]
+        s = '\n'.join(properties)
+        return '<{}\n{} \n>\n'.format(classname, s)
 
     def save(self):
-        pass
+        # log('debug save')
+        models = self.all()
+        # log('models', models)
+        # 如果没有 id，说明是新添加的元素
+        if self.id is None:
+            # 设置 self.id
+            # 先看看是否是空 list
+            if len(models) == 0:
+                # 我们让第一个元素的 id 为 1（当然也可以为 0）
+                self.id = 1
+            else:
+                m = models[-1]
+                # log('m', m)
+                self.id = m.id + 1
+            models.append(self)
+        else:
+            # index = self.find(self.id)
+            index = -1
+            for i, m in enumerate(models):
+                if m.id == self.id:
+                    index = i
+                    break
+            log('debug', index)
+            models[index] = self
+        l = [m.__dict__ for m in models]
+        path = self.db_path()
+        save(l, path)
 
 
+class User(Model):
+    def __init__(self, form):
+        self.id = form.get('id', None)
+        self.password = form.get('password', '')
+        self.username = form.get('username', '')
+
+    def salted_password(self, password, salt='$!@><?>HUI&DWQa`'):
+        import hashlib
+        def sha256(ascii_str):
+            """ Return the digest value as a string of hexadecimal digits. """
+            return hashlib.sha256(ascii_str.encode('ascii')).hexdigest()
+
+        hash1 = sha256(password)
+        hash2 = sha256(hash1 + salt)
+        return hash2
+
+    def hashed_password(self, pwd):
+        import hashlib
+        p = pwd.encode('ascii')
+        s = hashlib.sha256(p)
+        return s.hexdigest()
+
+    def validate_register(self):
+        pwd = self.password
+        self.password = self.salted_password(pwd)
+        if User.find_by(username=self.username) is None:
+            self.save()
+            return self
+        else:
+            return None
+
+    def validate_login(self):
+        u = User.find_by(username=self.username)
+        if u is not None:
+            return u.password == self.salted_password(self.password)
+        else:
+            return False
+
+    def todos(self):
+        return [t for t in Todo.all() if t.user_id == self.id]
+
+
+class Todo(Model):
+    def __init__(self, form, user_id=-1):
+        self.id = form.get('id', None)
+        self.task = form.get('task', '')
+        self.completed = False
+        self.user_id = form.get('user_id', user_id)
+
+    def __new__(cls, form, user_id=-1):
+        t = cls(form, user_id)
+        t.save()
+        return t
+
+    @classmethod
+    def update(cls, id, form):
+        t = cls.find(id)
+        valid_names = [
+            'task',
+            'completed'
+        ]
+        for key in form:
+            #
+            if key in valid_names:
+                setattr(t, key, form[key])
+        t.save()
+
+    @classmethod
+    def complete(cls, id, completed):
+        """
+        """
+        t = cls.find(id)
+        t.completed = completed
+        t.save()
+        return t
+
+    def is_owner(self, id):
+        return self.user_id == id
+
+
+#
+class Weibo(Model):
+    def __init__(self, form, user_id=-1):
+        self.id = form.get('id', None)
+        self.content = form.get('content', '')
+        self.user_id = form.get('user_id', user_id)
+
+    def comments(self):
+        # return [c for c in Comment.all() if c.weibo_id == self.id]
+        return Comment.find_all(weibo_id=self.id)
+
+
+class Comment(Model):
+    def __init__(self, form, user_id=-1):
+        self.id = form.get('id', None)
+        self.content = form.get('content', '')
+        self.user_id = form.get('user_id', user_id)
+        self.weibo_id = int(form.get('weibo_id', -1))
+
+    def user(self):
+        u = User.find_by(id=self.user_id)
+        return u
