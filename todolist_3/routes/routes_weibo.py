2933a850dd770d576bd757de81408352e927e829
cchangr <chen.changr@husky.neu.edu>
2018-11-20 16:27:23 -0500

    update

---
+++ todolist_3/routes/routes_weibo.py
@@ -0,0 +1,52 @@
+from todolist_3.models import User, Weibo
+from todolist_3.utils import redirect, template, http_response
+
+
+def login_required(route_fuction):
+    pass
+
+
+def index(request):
+    user_id = request.query.get('id', -1)
+    user_id = int(user_id)
+    user = User.find(user_id)
+    if user is None:
+        return redirect('/login')
+    weibos = Weibo.find_all(user_id=user_id)
+    body = template('weibo_index.html', weibos=weibos, user=user)
+    return http_response(body)
+
+
+def new(request):
+    pass
+
+
+def edit(request):
+    pass
+
+
+def add(request):
+    pass
+
+
+def update(request):
+    pass
+
+
+def delete(request):
+    pass
+
+
+def comment_add(request):
+    pass
+
+
+route_dict = {
+    '/weibo/index': index,
+    '/weibo/new': login_required(new),
+    '/weibo/edit': login_required(edit),
+    '/weibo/add': login_required(add),
+    '/weibo/update': login_required(update),
+    '/weibo/delete': login_required(delete),
+    '/comment/add': login_required(comment_add),
+}
--- todolist_3/utils.py
+++ todolist_3/utils.py
@@ -1,4 +1,6 @@
 import time
+import os
+import jinja2
 
 def log(*args, **kwargs):
     format = '%H:%M:%S'
@@ -8,5 +10,56 @@ def log(*args, **kwargs):
         print(dt, *args, file=f, **kwargs)
 
 
+path = '{}/templates/'.format(os.path.dirname(__file__))
+loader = jinja2.FileSystemLoader(path)
+env = jinja2.Environment(loader=loader)
 
 
+def template(path, **kwargs):
+    """
+    receive path and arguments
+    render template and return
+    """
+    t = env.get_template(path)
+    return t.render(**kwargs)
+
+
+def response_with_headers(headers, status_code=200):
+    header = 'HTTP/1.1 {} OK\r\n'.format(status_code)
+    header += ''.join(['{}: {}\r\n'.format(k, v)
+                           for k, v in headers.items()])
+    return header
+
+
+def redirect(location):
+    headers = {
+        'Content-Type': 'text/html',
+    }
+    headers['Location'] = location
+    # 301 永久重定向 302 普通定向
+    # 302 状态码的含义, Location 的作用
+    header = response_with_headers(headers, 302)
+    r = header + '\r\n' + ''
+    return r.encode(encoding='utf-8')
+
+
+def error(request, code=404):
+    """
+    404
+    """
+    e = {
+        404: b'HTTP/1.x 404 NOT FOUND\r\n\r\n<h1>NOT FOUND</h1>',
+    }
+    return e.get(code, b'')
+
+
+def http_response(body, headers=None):
+    """
+    headers HTTP
+    """
+    header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n'
+    if headers is not None:
+        header += ''.join(['{}: {}\r\n'.format(k, v)
+                            for k, v in headers.items()])
+    r = header + '\r\n' + body
+    return r.encode(encoding='utf-8')
